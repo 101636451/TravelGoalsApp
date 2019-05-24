253450df8d913870c95f50ab7b7718fe834468e3
Manoj Bandara <manojbandara@manojs-mbp.hub>
2019-05-25 00:14:23 +1000

    First Commit

---
+++ TravelGoals/TravelGoals/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TravelGoals
+//
+//  Created by Manoj Bandara on 25/5/19.
+//  Copyright © 2019 Manoj Bandara. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
